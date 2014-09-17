dc6c51e52bae49d55de37311c9acc504ec960665
normanj <norman@keepod.com>
2014-09-17 04:10:52 +0000

    add testing files

--- run_test.sh
+++ run_test.sh
@@ -3,5 +3,5 @@
 rm -rf /tmp/testrepo
 mkdir /tmp/testrepo
 
-repo init -u git://github.com/Keepod/keepodos.git
+cd /tmp/testrepo && repo init -u git://github.com/Keepod/keepodos.git
 cd /tmp/testrepo/.repo && cp manifests/test.xml manifest.xml && cd ../ && repo sync
